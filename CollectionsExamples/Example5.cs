d1b4ffbdaf96e0aaf7eab81bf279c64e8bd78c1c
Srikanth Rallabandi <codewithsrikanth@hotmail.com>
2024-03-21 21:17:27 +0530

    String class and extension methods

---
+++ CollectionsExamples/Example5.cs
@@ -0,0 +1,19 @@
+﻿namespace CollectionsExamples
+{
+    class Example5
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string,object> dict = new Dictionary<string,object>();
+            dict.Add("EmpID", 101);
+            dict.Add("EmpName", "Srikanth");
+            dict.Add("Salary", 1234.56);
+
+            Console.WriteLine("Elements of Dictionay are: ");
+            foreach (var item in dict.Keys)
+            {
+                Console.WriteLine(item+" => " + dict[item]);
+            }
+        }
+    }
+}
---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace ExtensionMethods
+{
+    public static class StringExtension
+    {
+        public static bool IsPalindrome(this string str)
+        {
+            str = str.Replace(" ", "").ToLower();
+            for (int i = 0; i < str.Length / 2; i++)
+            {
+                if (str[i] != str[str.Length - i - 1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string greeting = "Hello";
+            string name = "   Srikanth     ";
+            string palindrome = "leveljkk";
+
+            string interpolatedString = $"{greeting}, My name: {name}";
+            char firstChar = greeting[0];
+            int length = greeting.Length;
+            bool isContains = greeting.Contains("He");
+            string[] ar = interpolatedString.Split(' ');
+            bool isPlaindrom = palindrome.IsPalindrome();
+            Console.WriteLine(isPlaindrom);
+
+
+            Console.WriteLine(name.Length);
+            Console.WriteLine(name.Trim().Length);
+            foreach (var item in ar)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine(name.ToUpper());
+            Console.WriteLine(name.ToLower());
+            Console.WriteLine(interpolatedString);
+            Console.WriteLine(firstChar);
+            Console.WriteLine(length);
+            Console.WriteLine(isContains);
+
+        }
+    }
+}
